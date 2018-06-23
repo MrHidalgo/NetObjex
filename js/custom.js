8db88d2b6a730fb84e31ad89d101d7fdbf1b1617
MrHidalgo <Luckyhidalgo@gmail.com>
2018-06-23 12:03:49 +0300

    AI services page - adaptive. Add autoprefixer for style. Clean js code.

--- js/custom.js
+++ js/custom.js
@@ -337,17 +337,18 @@ $(function () {
       }
     ]
   });
-});
 
-$("[process-btn-js ]").on("click", function(e) {
-  e.preventDefault();
 
-  var elem = $(e.target),
-    elemAttr = elem.attr("data-btn");
+  $("[process-btn-js ]").on("click", function(e) {
+    e.preventDefault();
 
-  $("[process-btn-js ]").removeClass("is-active");
-  elem.addClass("is-active");
+    var elem = $(e.target),
+      elemAttr = elem.attr("data-btn");
 
-  $(".process__body").removeClass("is-active");
-  $(".process__body[data-body='" + elemAttr + "']").addClass("is-active");
+    $("[process-btn-js ]").removeClass("is-active");
+    elem.addClass("is-active");
+
+    $(".process__body").removeClass("is-active");
+    $(".process__body[data-body='" + elemAttr + "']").addClass("is-active");
+  });
 });
